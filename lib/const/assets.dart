224a3cd3c8c2d56bab2709d7f2bd9945a6615269
Abhiram T Sabu <sandweepsabu@gmail.com>
2024-12-01 17:20:25 +0530

    created the splash screen and added all the logo assets

---
+++ lib/const/assets.dart
@@ -0,0 +1,6 @@
+const String enter = "assets/enter.png";
+const String female = "assets/female.png";
+const String male = "assets/male.png";
+const String fitchecklogo = "assets/fitchecklogo.png";
+const String reload = "assets/reload.png";
+const String snackLogo = "assets/snackLogo.png";
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:fit_check_app/const/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(fitchecklogo),
+        ),
+      ),
+    );
+  }
+}
